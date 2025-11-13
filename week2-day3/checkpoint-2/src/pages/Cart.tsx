16d28d4ce7da773bb675488fa4325650e1104e12
mixx-code <rfebriansyah385@gmail.com>
2025-11-13 21:31:51 +0700

    tambahan sedikit pada chackpoint 2

--- week2-day3/checkpoint-2/src/pages/Cart.tsx
+++ week2-day3/checkpoint-2/src/pages/Cart.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
     Table,
     TableBody,
@@ -6,27 +7,45 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     return (
+      <>
+        <Button asChild variant="secondary" className="mb-4">
+          <Link to="/">Back to Home</Link>
+        </Button>
+        <div className="flex justify-content-start">
+          <h1 className="text-2xl font-bold mb-5">Cart</h1>
+        </div>
         <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">No</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                    <TableHead className="text-right">Qty</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow>
-                    <TableCell className="font-medium">INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    <TableCell className="text-right">1</TableCell>
-                </TableRow>
-            </TableBody>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">No</TableHead>
+              <TableHead className="text-center">Name</TableHead>
+              <TableHead className="text-center">Status</TableHead>
+              <TableHead className="text-center">Amount</TableHead>
+              <TableHead className="text-center">Qty</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell className="text-center">Ganci</TableCell>
+              <TableCell className="text-center">Paid</TableCell>
+              <TableCell className="text-center">Rp. 30000</TableCell>
+              <TableCell className="text-center">1</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">INV002</TableCell>
+              <TableCell className="text-center">Galon</TableCell>
+              <TableCell className="text-center">Paid</TableCell>
+              <TableCell className="text-center">Rp. 19000</TableCell>
+              <TableCell className="text-center">1</TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
+      </>
     );
 }
 
